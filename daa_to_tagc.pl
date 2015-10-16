89b276883c7a6f57a2973e615918a2d201de8ff7
GDKO <gdkoutsovoulos@gmail.com>
2015-10-16 17:23:00 +0100

    Added error messages

--- daa_to_tagc.pl
+++ daa_to_tagc.pl
@@ -3,8 +3,14 @@
 use strict;
 use warnings;
 
+my $tool_name = "diamond";
+my $tool_path = `which $tool_name`;
+die "No $tool_name command available\n" unless ( $tool_path );
+
 my $uniref_file=$ARGV[0];
 my $diamond_file=$ARGV[1];
+
+die "Usage: daa_to_tagc.pl uniref100.taxlist [assembly_se_uniref.daa]\n" unless ( $uniref_file && $diamond_file );
 
 open (IN,"$uniref_file");
 
